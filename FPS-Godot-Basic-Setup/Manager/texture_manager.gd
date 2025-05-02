f2097dd076166851f44ab9ed5d55eccde02a7f68
Justin Morneau <jmorneau25@gmail.com>
2025-05-02 02:29:50 -0400

    Add generated icon texture and update Apple scene references

--- FPS-Godot-Basic-Setup/Manager/texture_manager.gd
+++ FPS-Godot-Basic-Setup/Manager/texture_manager.gd
@@ -1,1 +1,32 @@
 extends Node
+
+const POLYGON_DEFAULT = preload("res://Assets/PolygonShops_Texture_01_A.png")
+const POLYGON_A = preload("res://Assets/PolygonShops_Texture_01_B.png")
+const POLYGON_B = preload("res://Assets/PolygonShops_Texture_01_C.png")
+const POLYGON_C = preload("res://Assets/PolygonShops_Texture_02_A.png")
+const POLYGON_D = preload("res://Assets/PolygonShops_Texture_02_B.png")
+const POLYGON_E = preload("res://Assets/PolygonShops_Texture_02_C.png")
+const POLYGON_F = preload("res://Assets/PolygonShops_Texture_03_A.png")
+const POLYGON_G = preload("res://Assets/PolygonShops_Texture_03_B.png")
+const POLYGON_H = preload("res://Assets/PolygonShops_Texture_03_C.png")
+const POLYGON_I = preload("res://Assets/PolygonShops_Texture_04_A.png")
+const POLYGON_J = preload("res://Assets/PolygonShops_Texture_04_B.png")
+const POLYGON_K = preload("res://Assets/PolygonShops_Texture_04_C.png")
+
+var polygon_texture_array: Array[Texture2D] = []
+
+func _ready() -> void:
+	polygon_texture_array = [
+		POLYGON_DEFAULT,
+		POLYGON_A,
+		POLYGON_B,
+		POLYGON_C,
+		POLYGON_D,
+		POLYGON_E,
+		POLYGON_F,
+		POLYGON_G,
+		POLYGON_H,
+		POLYGON_I,
+		POLYGON_J,
+		POLYGON_K
+	]
